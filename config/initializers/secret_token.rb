84de57d96dd45407d03d40d75fa33a00cadecc77
unknown <oliver.hofmann@fhws.de>
2013-02-24 13:07:15 +0100

    Neues Rails Projekt angelegt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SS13FWPMRuby::Application.config.secret_token = 'd4868c15a24864686c6f31ab1b2dfc679aca211b3305565502812ad2d41c29a7e43a9409347df1a48068b0933d9878411c7a9d246697e49b22ce25c097caba43'
